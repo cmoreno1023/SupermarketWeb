78c7f4b03382ac67f376beb6dd639a19f013917b
cmoreno1023 <cmoreno2078@gmail.com>
2024-10-30 14:54:57 -0500

    Delete Categories

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea40c0a820a8b68dc25c57492126aa95e2cc2fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b987967333f9a26cff3acaa8266815e062fead")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
